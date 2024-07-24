1ab35927ea7eca891761eda60b3ee92dcada76b5
Arun7753 <arun775398@gmail.com>
2024-07-25 01:21:57 +0530

    new

---
+++ main.js
@@ -0,0 +1,12 @@
+// const box1 = document.getElementById('box1');
+// const box2 = document.getElementById('Box2');
+// const box3 = document.getElementById('Box3');
+// const box4 = document.getElementById('Box4');
+// const box5 = document.getElementById('Box5');
+
+// function changingcolor(){
+//     const boxcolor = window.getComputedStyle(box1).backgroundColor
+//     document.body.style.backgroundColor = boxcolor
+// }
+
+// box1.addEventListener('click', changingcolor)
